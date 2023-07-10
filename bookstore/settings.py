3381135dc3eb9cba95fc26b799b4b1602bae325c
ajbbents <107074385+ajbbents@users.noreply.github.com>
2023-07-10 15:01:22 -0500

    adjust settings

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-#eo+owi*=g*4r9zlo1sfa&5(f*3e$(y(@1=&u1vz$j%jph)4@6')
+SECRET_KEY = os.environ.get(
+    'DJANGO_SECRET_KEY', 'here is a weird secret key that is magic.')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -141,6 +143,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = '/login/'
 
 # Heroku: Update database configuration from $DATABASE_URL.
-import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
